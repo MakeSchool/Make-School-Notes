d3d2901d61bef7a204e792533010d8424ebe1954
Ben-G <Benjamin.Encz@gmail.com>
2015-02-13 14:19:17 -0800

    Add custom dismiss segue

---
+++ MakeSchoolLibrary/MakeSchoolLibrary.h
@@ -0,0 +1,19 @@
+//
+//  MakeSchoolLibrary.h
+//  MakeSchoolLibrary
+//
+//  Created by Benjamin Encz on 2/13/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MakeSchoolLibrary.
+FOUNDATION_EXPORT double MakeSchoolLibraryVersionNumber;
+
+//! Project version string for MakeSchoolLibrary.
+FOUNDATION_EXPORT const unsigned char MakeSchoolLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MakeSchoolLibrary/PublicHeader.h>
+
+
