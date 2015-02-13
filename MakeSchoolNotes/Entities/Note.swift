829ab24aefa0ea14a13ca2988dfa67249d98a4ac
Ben-G <Benjamin.Encz@gmail.com>
2015-02-13 11:02:39 -0800

    Basic set up

---
+++ MakeSchoolNotes/Entities/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  MakeSchoolNotes
+//
+//  Created by Benjamin Encz on 2/13/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+  
+}
---
+++ MakeSchoolNotes/Entities/NoteBook.swift
@@ -0,0 +1,13 @@
+//
+//  Notebook.swift
+//  MakeSchoolNotes
+//
+//  Created by Benjamin Encz on 2/13/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import Foundation
+
+class NoteBook {
+  
+}
