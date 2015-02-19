14f36038a8fb8817e95996f6897d28be1b3ef700
Ben-G <Benjamin.Encz@gmail.com>
2015-02-18 17:32:18 -0800

    Remove padding on textview

---
+++ MakeSchoolNotes/ViewControllers/NewNoteViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewNoteViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Benjamin Encz on 2/13/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class NewNoteViewController: UIViewController {
+
+}
